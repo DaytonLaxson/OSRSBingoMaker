8c04fad5aa8ffac6dee2e5d23376d4f84282583a
Juder Stidham <juder.stidham@gmail.com>
2025-05-05 21:24:35 -0500

    refactoring code a bit, added express for api handling

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the client/public directory
+app.use(express.static(path.join(__dirname, '../client/public')));
+
+// API routes can go here
+app.get('/api/status', (req, res) => {
+  res.json({ status: 'Server is running!' });
+});
+
+// For any other GET request, send the index.html
+app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
